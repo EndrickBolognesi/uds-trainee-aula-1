c58d41de30b7e8c7706f22ceaab42ab03e28f83f
= <=>
2018-06-09 16:14:54 -0300

    Exercicio hardcore 2

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,152 @@
-import javax.swing.*;
 import java.util.Scanner;
+
 class Main {
-        public static String romanos(Integer numero){
-                switch(numero){
-                    case 1: return "I";
-                    case 2: return "II";
-                    case 3: return "III";
-                    case 4: return "IV";
-                    case 5: return "V";
-                    case 6: return "VI";
-                    case 7: return "VII";
-                    case 8: return "VIII";
-                    case 9: return "IX";
-                    case 10: return "X";
-                    default: return "Numero invalido";
-
-                }
+    public static void armas(float saldo, int categoria, int produto) {
+        System.out.println("ARMAS " + "         SALDO: " + saldo + " \n " +
+                "1 > AK-47 - 1000" + " \n " +
+                "2 > M4-A1 - 900" + " \n " +
+                "3 > RPG   - 800" + " \n " +
+                "4 > AWP   - 500" + " \n " +
+                "5 > AUG   - 600" + " \n "
+        );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escolha uma arma");
+        categoria = 0;
+        categoria = scanner.nextInt();
+        switch(categoria){
+            case 1:  saldo -= 1000; menu(saldo, categoria, produto);
+            case 2:  saldo -= 900; menu(saldo, categoria, produto);
+            case 3:  saldo -= 800; menu(saldo, categoria, produto);
+            case 4:  saldo -= 500; menu(saldo, categoria, produto);
+            case 5:  saldo -= 600; menu(saldo, categoria, produto);
         }
-    public static void main(String[] args) {
-        int numero = 10;
-//        for (int i = 10; i >= 1; i--){
-//        numero = i;
-//
-//        System.out.println(romanos(numero));
-//        JOptionPane.showMessageDialog(null,romanos(numero), "Resultado" , JOptionPane.WARNING_MESSAGE);
-//        }
-
-        while( numero >= 1){
-            JOptionPane.showMessageDialog(null,romanos(numero), "Resultado" , JOptionPane.WARNING_MESSAGE);
-        numero--;
-
-        }
-        /*int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
-
-        JOptionPane.showMessageDialog(null,romanos(numero), "Resultado" , JOptionPane.WARNING_MESSAGE);*/
 
     }
+
+    public static void drogas(float saldo, int categoria, int produto) {
+        System.out.println("DROGAS " + "         SALDO: " + saldo + " \n " +
+                "1 > PHP - 1000" + " \n " +
+                "2 > JAVA -0" + " \n " +
+                "3 > C++" + " \n " +
+                "4 > DELPHI" + " \n " +
+                "5 > JAVASCRIPT" + " \n "
+        );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escolha uma arma");
+        categoria = 0;
+        categoria = scanner.nextInt();
+        switch(categoria){
+            case 1:  saldo -= 1000; menu(saldo, categoria, produto);
+            case 2:  saldo -= 900; menu(saldo, categoria, produto);
+            case 3:  saldo -= 800; menu(saldo, categoria, produto);
+            case 4:  saldo -= 500; menu(saldo, categoria, produto);
+            case 5:  saldo -= 600; menu(saldo, categoria, produto);
+        }
+    }
+
+
+    public static void veiculos(float saldo, int categoria, int produto) {
+        System.out.println("VEICULOS " + "         SALDO: " + saldo + " \n " +
+                "1 > FERRARI" + " \n " +
+                "2 > LAMBO" + " \n " +
+                "3 > AUDI" + " \n " +
+                "4 > FIAT" + " \n " +
+                "5 > TOYOTA" + " \n "
+        );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escolha uma arma");
+        categoria = 0;
+        categoria = scanner.nextInt();
+        switch(categoria){
+            case 1:  saldo -= 1000; menu(saldo, categoria, produto);
+            case 2:  saldo -= 900; menu(saldo, categoria, produto);
+            case 3:  saldo -= 800; menu(saldo, categoria, produto);
+            case 4:  saldo -= 500; menu(saldo, categoria, produto);
+            case 5:  saldo -= 600; menu(saldo, categoria, produto);
+        }
+
+    }
+
+    public static void extras(float saldo, int categoria, int produto) {
+        System.out.println("EXTRAS " + "         SALDO: " + saldo + " \n " +
+                "1 > +VIDAS" + " \n " +
+                "2 > MANA" + " \n " +
+                "3 > VIDA" + " \n " +
+                "4 > XP" + " \n " +
+                "5 > ESTAMINA" + " \n "
+        );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escolha uma arma");
+        categoria = 0;
+        categoria = scanner.nextInt();
+        switch(categoria){
+            case 1:  saldo -= 1000; menu(saldo, categoria, produto);
+            case 2:  saldo -= 900; menu(saldo, categoria, produto);
+            case 3:  saldo -= 800; menu(saldo, categoria, produto);
+            case 4:  saldo -= 500; menu(saldo, categoria, produto);
+            case 5:  saldo -= 600; menu(saldo, categoria, produto);
+        }
+
+    }
+
+    public static void lixo(float saldo, int categoria, int produto) {
+        System.out.println("LIXO " + "         SALDO: " + saldo + " \n " +
+                "1 > LIXO" + " \n " +
+                "2 > LIXO" + " \n " +
+                "3 > LIXO" + " \n " +
+                "4 > LIXO" + " \n " +
+                "5 > LIXO" + " \n "
+        );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escolha uma arma");
+        categoria = 0;
+        categoria = scanner.nextInt();
+        switch(categoria){
+            case 1:  saldo -= 1000; menu(saldo, categoria, produto);
+            case 2:  saldo -= 900; menu(saldo, categoria, produto);
+            case 3:  saldo -= 800; menu(saldo, categoria, produto);
+            case 4:  saldo -= 500; menu(saldo, categoria, produto);
+            case 5:  saldo -= 600; menu(saldo, categoria, produto);
+        }
+
+    }
+
+    public static void menu(float saldo, int categoria, int produto){
+
+        System.out.println("CATEGORIAS " + "         SALDO: " + saldo + " \n " +
+                "1 > ARMAS" + " \n " +
+                "2 > DROGAS" + " \n " +
+                "3 > VEICULOS" + " \n " +
+                "4 > EXTRAS" + " \n " +
+                "5 > LIXO" + " \n " +
+                "6 > SAIR" + " \n "
+
+        );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Selecione uma Categoria");
+        categoria = scanner.nextInt();
+        switch(categoria){
+            case 1: armas(saldo, categoria, produto);
+            case 2: drogas(saldo, categoria, produto);
+            case 3: veiculos(saldo, categoria, produto);
+            case 4: extras(saldo, categoria, produto);
+            case 5: lixo(saldo, categoria, produto);
+            case 6: System.exit(0) ;
+
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        float saldo = 1500.00f;
+        int categoria = 0;
+        int produto = 0;
+
+        menu(saldo, categoria, produto);
+    }
+
+
+
+
 }
